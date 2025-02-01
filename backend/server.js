08bdcf9acfb04c240b9f0f6dec797165811b7919
iambhattaraitanuj <iamtanujbhattarai@gmail.com>
2025-02-01 13:39:51 +0545

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import bodyParser from "body-parser";
-import serverless from "serverless-http";
 
 const port = 3001;
 mongoose.connect("mongodb://localhost:27017/todolist");
@@ -20,5 +19,6 @@ app.get("/", async (req, res) => {
   res.send("I am server");
 });
 
-// Export the serverless function handler for Vercel
-export const handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Server starting on http://localhost:${port}`);
+});
